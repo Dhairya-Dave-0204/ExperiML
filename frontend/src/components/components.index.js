d559127ec194138e2423c54e86cfc5f3caa17b04
Dhairya Dave <dhairyadave962@gmail.com>
2026-07-21 10:42:49 +0530

    Create the static landing page and connect it to Home

---
+++ frontend/src/components/components.index.js
@@ -0,0 +1,5 @@
+import LandingPage from "./shared/Landing/LandingPage";
+
+export {
+    LandingPage
+}
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { LandingPage } from "../../components/components.index"
+
+function Home() {
+  return (
+    <LandingPage />
+  )
+}
+
+export default Home
