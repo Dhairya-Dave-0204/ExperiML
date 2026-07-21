1fd6e0225634e2db036b7d461fcd701af2c0d36e
Dhairya Dave <dhairyadave962@gmail.com>
2026-07-21 11:11:08 +0530

    Create constants and create the setup for file imports

---
+++ frontend/src/constants/app.js
@@ -0,0 +1,5 @@
+export const APP = {
+  NAME: "ExperiML",
+  DESCRIPTION: "Machine Learning Experiment Management Platform",
+  VERSION: "1.0.0",
+};
--- frontend/src/routes/AppRoutes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "../pages/Home";
+import { Home } from "../../pages/page.index";
 
 
 export default function AppRoutes(){
