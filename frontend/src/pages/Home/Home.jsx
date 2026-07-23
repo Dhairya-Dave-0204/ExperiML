f5e430c2e0bab85d465e6ddfc2ca0b5d506ee09b
Dhairya Dave <dhairyadave962@gmail.com>
2026-07-23 10:52:46 +0530

    Connect the workspace component to the homw page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from "react";
 
-import { Hero, Narrative, Capabilities, Reproducibility, ExperimentLineage } from "@/components/components.index"
+import {
+  Hero,
+  Narrative,
+  Capabilities,
+  Reproducibility,
+  ExperimentLineage,
+  Workspace,
+} from "@/components/components.index";
 
 function Home() {
   return (
@@ -10,8 +17,9 @@ function Home() {
       <Capabilities />
       <Reproducibility />
       <ExperimentLineage />
+      <Workspace />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
