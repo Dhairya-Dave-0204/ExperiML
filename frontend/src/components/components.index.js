6c04363af6c37c196f270c06d8904e84bcb42406
Dhairya Dave <dhairyadave962@gmail.com>
2026-07-27 15:50:03 +0530

    Create and export faq accordian component

--- frontend/src/components/components.index.js
+++ frontend/src/components/components.index.js
@@ -35,6 +35,7 @@ export { default as Locationmap } from "./shared/Contact/Locationmap/Locationmap
 export { default as DeveloperSection } from "./shared/Contact/DeveloperSection/DeveloperSection"
 export { default as ContactReasons } from "./shared/Contact/ContactReasons/ContactReasons"
 export { default as ContactCTA } from "./shared/Contact/ContactCTA/ContactCTA"
+export { default as FaqAccordion } from "./shared/FAQ/FAQAccordion/FaqAccordion"
 
 // FAQ page components
 export { default as FaqMainLayout } from "./shared/FAQ/FaqMainLayout/FaqMainLayout"
