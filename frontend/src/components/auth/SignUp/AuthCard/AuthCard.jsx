4b85caf12186271b02d2afd3464b505e7878aa1b
Dhairya Dave <dhairyadave962@gmail.com>
2026-07-28 18:55:10 +0530

    Increasse width of the sign up form

--- frontend/src/components/auth/SignUp/AuthCard/AuthCard.jsx
+++ frontend/src/components/auth/SignUp/AuthCard/AuthCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function AuthCard({ children }) {
   return (
-    <div className="relative w-full max-w-lg p-8 border shadow-lg rounded-2xl border-border bg-surface sm:p-10">
+    <div className="relative w-full max-w-xl p-8 border shadow-lg rounded-2xl border-border bg-surface sm:p-10">
       {children}
     </div>
   );
