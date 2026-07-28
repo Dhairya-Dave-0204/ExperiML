4e75e47785442608ede895fa2ac874f2193d1d4e
Dhairya Dave <dhairyadave962@gmail.com>
2026-07-28 16:45:34 +0530

    Connect the scroll to top to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import { AppRoutes } from "@/routes/routes.index"
 
+import { ScrollToTop } from "@/components/components.index"
+
 function App() {
 
   return (
     <>
+      <ScrollToTop />
+
       <AppRoutes />
     </>
   )
