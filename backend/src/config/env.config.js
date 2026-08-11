d32997ced501baf6b665d737e14f3bb0bfebb4fa
Dhairya-Dave-0204 <dhairyadave962@gmail.com>
2026-08-11 11:14:23 +0530

    Centralize the env imports in the config/env.js file

--- backend/src/config/env.config.js
+++ backend/src/config/env.config.js
@@ -1,4 +1,8 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
 
 export const env = {
   NODE_ENV: process.env.NODE_ENV || "development",
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,7 @@
-import dotenv from "dotenv";
 import { app } from "./app.js";
 import { env } from "#config/env.config";
 
-dotenv.config({
-  path: "./.env",
-});
+
 
 process.on("uncaughtException", (err) => {
   console.error("UNCAUGHT EXCEPTION!");
