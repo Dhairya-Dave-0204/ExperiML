9f8ccedbc4319801a9b022f418df7818b10f458c
Dhairya-Dave-0204 <dhairyadave962@gmail.com>
2026-08-21 10:29:59 +0530

    Create the seperation of routes for whole application

--- frontend/src/routes/routes.index.js
+++ frontend/src/routes/routes.index.js
@@ -1,1 +1,3 @@
-export { default as  AppRoutes } from "./AppRoutes/AppRoutes";
+export { default as AppRoutes } from "./AppRoutes/AppRoutes";
+export { default as AuthRoutes } from "./AuthRoutes/AuthRoutes";
+export { default as PublicRoutes } from "./PublicRoutes/PublicRoutes";
