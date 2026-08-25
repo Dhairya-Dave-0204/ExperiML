2120112c9018d6c23a91b1021bab0b95e4d4eb5e
Dhairya-Dave-0204 <dhairyadave962@gmail.com>
2026-08-25 14:27:39 +0530

    Create a dshboard page boiler plate and add it to central export

--- frontend/src/pages/page.index.js
+++ frontend/src/pages/page.index.js
@@ -1,3 +1,4 @@
+// Public pages
 export { default as Home } from "./public/Home/Home";
 export { default as About } from "./public/About/About";
 export { default as Contact } from "./public/Contact/Contact";
@@ -6,8 +7,13 @@ export { default as Documentation } from "./public/Documentation/Documentation";
 export { default as PrivacyPolicy } from "./public/PrivacyPolicy/PrivacyPolicy";
 export { default as DataPolicy } from "./public/DataPolicy/DataPolicy";
 export { default as CookiePolicy } from "./public/CookiePolicy/CookiePolicy";
+export { default as TermsOfService } from "./public/TermsOfService/TermsOfService";
+export { default as NotFound } from "./public/NotFound/NotFound";
+
+// Authentication pages
+export { default as ForgotPassword } from "./auth/ForgotPassword/ForgotPassword";
 export { default as SignIn } from "./auth/SignIn/SignIn";
 export { default as SignUp } from "./auth/SignUp/SignUp";
-export { default as TermsOfService } from "./public/TermsOfService/TermsOfService";
-export { default as ForgotPassword } from "./auth/ForgotPassword/ForgotPassword";
-export { default as NotFound } from "./public/NotFound/NotFound";
+
+// Main application pages
+export { default as Dashboard } from "./app/Dashboard/Dashboard";
