d3a169be82c4a5847e1e52d72455c3ea639e57ab
Dhairya-Dave-0204 <dhairyadave962@gmail.com>
2026-08-25 12:10:12 +0530

    Add the global high level routes to the app routes'

--- frontend/src/routes/AppRoutes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes/AppRoutes.jsx
@@ -1,22 +1,22 @@
 import { Route } from "react-router-dom";
 
 import { AppLayout } from "@/layout/layout.index";
-
+import { ROUTES } from "@/constants/routes";
 
 const AppRoutes = (
   <Route element={<AppLayout />}>
+    <Route path={ROUTES.APP} element={<div className="p-10">Dashboard</div>} />
 
     <Route
-      path="/app"
-      element={
-        <div className="p-10">
-          Protected App Route
-        </div>
-      }
+      path={ROUTES.PROJECTS}
+      element={<div className="p-10">Projects</div>}
     />
 
+    <Route
+      path={ROUTES.SETTINGS}
+      element={<div className="p-10">Settings</div>}
+    />
   </Route>
 );
-
 
 export default AppRoutes;
