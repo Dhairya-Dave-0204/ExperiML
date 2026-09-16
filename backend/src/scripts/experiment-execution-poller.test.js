8110fd0ff4cddf0043082fb71d449c6ff0b97aa2
Dhairya-Dave-0204 <dhairyadave962@gmail.com>
2026-09-16 12:12:11 +0530

    Update the execution id for testing the execution poller

--- backend/src/scripts/experiment-execution-poller.test.js
+++ backend/src/scripts/experiment-execution-poller.test.js
@@ -1,6 +1,6 @@
 import experimentExecutionPollerService from "#services/experiment-execution-poller.service";
 
-const EXECUTION_ID = "8e9331d2-fb6a-4a5a-b752-881f6de30d03";
+const EXECUTION_ID = "b734df86-97b3-4b6d-8723-f1767408ed84";
 
 async function main() {
   console.log("Starting experiment execution polling test...\n");
