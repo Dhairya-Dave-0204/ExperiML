fb2433579d39f258dfcb5c79aa2bf60706540571
Dhairya-Dave-0204 <dhairyadave962@gmail.com>
2026-09-23 14:55:54 +0530

    fix the sidebar responsive behaviour

--- frontend/src/layout/AppLayout/AppLayout.jsx
+++ frontend/src/layout/AppLayout/AppLayout.jsx
@@ -4,10 +4,10 @@ import { AppSidebar } from "@/components/components.index";
 
 function AppLayout() {
   return (
-    <div className="flex min-h-screen bg-background lg:h-screen lg:overflow-hidden lg:flex-row">
+    <div className="flex flex-col min-h-screen bg-background lg:h-screen lg:flex-row lg:overflow-hidden">
       <AppSidebar />
 
-      <main className="flex-1 min-w-0 lg:overflow-y-auto">
+      <main className="flex-1 w-full min-w-0 lg:overflow-y-auto">
         <Outlet />
       </main>
     </div>
