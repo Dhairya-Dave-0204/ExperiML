16598c649e99811cadc147aeafaf32d9b954cbd9
Dhairya-Dave-0204 <dhairyadave962@gmail.com>
2026-09-23 15:47:54 +0530

    Connect the global page to the routes

--- frontend/src/routes/AppRoutes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes/AppRoutes.jsx
@@ -2,16 +2,18 @@ import { Route } from "react-router-dom";
 
 import { AppLayout } from "@/layout/layout.index";
 import { ROUTES } from "@/constants/routes";
-import { Dashboard, Settings, ProjectOverview } from "@/pages/page.index";
+import {
+  Dashboard,
+  Projects,
+  Settings,
+  ProjectOverview,
+} from "@/pages/page.index";
 
 const AppRoutes = (
   <Route element={<AppLayout />}>
     <Route path={ROUTES.APP} element={<Dashboard />} />
 
-    <Route
-      path={ROUTES.PROJECTS}
-      element={<div className="p-10">Projects</div>}
-    />
+    <Route path={ROUTES.PROJECTS} element={<Projects />} />
 
     <Route path={ROUTES.SETTINGS} element={<Settings />} />
 
